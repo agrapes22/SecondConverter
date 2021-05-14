76b4a2abeba2650c8bdb1746fe0177ff4ebdb9c5
agrapes22 <agrapes22@gmail.com>
2021-05-13 20:38:40 -0700

    Initial commit
    Convert seconds to mins, hours, days as appropriate.

--- Exercise4/Form1.Designer.cs
+++ Exercise4/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.secondsBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.answerText = new System.Windows.Forms.Label();
+            this.errorMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(344, 177);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter number of seconds to convert:";
+            // 
+            // secondsBox
+            // 
+            this.secondsBox.Location = new System.Drawing.Point(308, 130);
+            this.secondsBox.Name = "secondsBox";
+            this.secondsBox.Size = new System.Drawing.Size(153, 20);
+            this.secondsBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(328, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Converted:";
+            // 
+            // answerText
+            // 
+            this.answerText.AutoSize = true;
+            this.answerText.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.answerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerText.Location = new System.Drawing.Point(0, 296);
+            this.answerText.Name = "answerText";
+            this.answerText.Padding = new System.Windows.Forms.Padding(320, 0, 0, 150);
+            this.answerText.Size = new System.Drawing.Size(369, 165);
+            this.answerText.TabIndex = 4;
+            this.answerText.Text = "15 days";
+            this.answerText.Visible = false;
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.AutoSize = true;
+            this.errorMessage.ForeColor = System.Drawing.Color.Red;
+            this.errorMessage.Location = new System.Drawing.Point(305, 153);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(163, 13);
+            this.errorMessage.TabIndex = 5;
+            this.errorMessage.Text = "Input is not a number in seconds!";
+            this.errorMessage.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.errorMessage);
+            this.Controls.Add(this.answerText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.secondsBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox secondsBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label answerText;
+        private System.Windows.Forms.Label errorMessage;
     }
 }
 
--- Exercise4/Form1.cs
+++ Exercise4/Form1.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**
+ * Program takes input integer and converts seconds to minutes, hours, and days as appropriate.
+ * Will convert to minutes under one hour, hours under one day, and days over one day.
+ * Pairs with proper units and handles input errors.
+ * */
+ 
 namespace Exercise4
 {
     public partial class Form1 : Form
@@ -15,6 +21,68 @@ namespace Exercise4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            //hide error message if visible from prior run
+            errorMessage.Visible = false;
+
+            //check if blank
+            if (!String.IsNullOrWhiteSpace(secondsBox.Text))
+            {
+                int seconds = 0;
+                //check if the input is an integer
+                if (int.TryParse(secondsBox.Text, out seconds) == true)
+                {
+                    double time = 0;
+                    String unit = "";
+
+                    //convert to minutes
+                    if (seconds >= 60 && seconds < 3600)
+                    {
+                        time = (int)seconds / 60;
+                        if (time < 2)
+                            unit = " minute";
+                        else
+                            unit = " minutes";
+                    }
+                    //convert to hours
+                    else if (seconds >= 3600 && seconds < 86400)
+                    {
+                        time = (int)seconds / 3600;
+                        if (time < 2)
+                            unit = " hour";
+                        else
+                            unit = " hours";
+                    }
+                    //convert to days
+                    else if (seconds >= 86400)
+                    {
+                        time = (int)seconds / 86400;
+                        if (time < 2)
+                            unit = " day";
+                        else
+                            unit = " days";
+                    }
+                    //just output as seconds
+                    else
+                    {
+                        time = seconds;
+                        unit = " seconds";
+                    }
+
+                    //reveal the answer text, set to string to tell how many seconds in the unit
+                    answerText.Visible = true;
+                    answerText.Text = time.ToString() + unit + " in " + seconds + " seconds";
+                }
+                //output an error if not an integer
+                else
+                {
+                    answerText.Visible = false;
+                    errorMessage.Visible = true;
+                }
+            }
         }
     }
 }
